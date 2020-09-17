4017ecfb7c192274a60ca9d2859fa990f39d36d4
Aurel Marishta <aurel.marishta@gmail.com>
2020-09-17 12:27:52 +0200

    initial commit

---
+++ config-server/src/test/java/al/aurel/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package al.aurel.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
